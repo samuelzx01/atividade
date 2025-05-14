7e1006235f1d0adb98f3ac60f0c37ab6baf62114
samuelzx01 <samuelsoares200@gmail.com>
2025-05-14 20:21:53 -0300

    atualizando

--- projeto.py
+++ projeto.py
@@ -1,3 +1,28 @@
+opcoes = input('1 - cadastrar\n2 - listar\n3 - atualizar\n4 - remover\n5 - Sair')
+while True:
+    if (opcoes == '5'):
+        print('Saindo...')
+        break
+    if (opcoes == '1'):
+         produto = input('digite o nme do produto')
+         preco = input('digite o valor do produto')
+         print('produto inserido com sucesso')
+
+    if (opcoes == 2):
+        produtos = listar_produtos()
+
+        for produto in produtos:
+            print(f'ID:{produto['id_produto']}')
+
+    if(opcoes == 3):
+        ...
+
+    
+         
+
+
+
+
 #criação do banco e conexao
 
 
@@ -28,4 +53,4 @@
 
 
 
-# 8. Sair 
+# 5. Sair 
