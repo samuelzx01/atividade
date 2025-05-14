c719069002a317171495794bb72aca8fec67fab8
AlexyaCC50 <huygneszindocsgo@gmail.com>
2025-05-14 19:32:30 -0300

    fff

--- projeto.py
+++ projeto.py
@@ -1,14 +1,2 @@
 #  Opa
 
-import mysql as my
-
-def conectar_banco():
-    connect = my.connect(
-        host = 'localhost',
-        user = 'root',
-        password = '',
-        database = 'loja',
-
-    )
-
-
