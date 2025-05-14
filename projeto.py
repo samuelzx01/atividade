145ebcf368afd0f1557d3c1fc254d6d502fde1f3
samuelzx01 <samuelsoares200@gmail.com>
2025-05-14 19:24:51 -0300

    atividade

--- projeto.py
+++ projeto.py
@@ -1,1 +1,31 @@
-#  opa
+#criação do banco e conexao
+
+
+
+# 1. Cadastrar novo produto 
+
+
+
+
+
+
+# 2. Listar todos os produtos 
+
+
+
+
+
+# 3. Atualizar produto 
+
+
+
+
+
+# 4. Remover produto 
+
+
+
+
+
+
+# 8. Sair 
