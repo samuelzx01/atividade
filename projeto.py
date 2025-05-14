339b2e324f18f659b4d2f04beaa57f53c7cc158d
AlexyaCC50 <huygneszindocsgo@gmail.com>
2025-05-14 19:06:55 -0300

    umpdate 0.1

--- projeto.py
+++ projeto.py
@@ -1,1 +1,14 @@
-#  opa
+#  Opa
+
+import mysql as my
+
+def conectar_banco():
+    connect = my.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '',
+        database = 'loja',
+
+    )
+
+
